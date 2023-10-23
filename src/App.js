9a721d66a9213caf1829fefa7133fdc3209387d9
shihabhassannaim <hassanshihab231@gmail.com>
2023-10-23 10:11:24 +0600

    Day-43

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import data from './search_filter.json'
 
 function App() {
+  const [cng , setCng] = useState("");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" placeholder='Search....' onChange={(event) =>setCng(event.target.value)} />
+      <ul>{data.filter((user) => 
+        user.first_name.toLowerCase().includes(cng)
+      ).map(user => (
+        <li>{user.first_name}</li>
+        ))}
+        </ul>
     </div>
   );
 }
